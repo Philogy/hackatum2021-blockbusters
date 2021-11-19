478b3f222d7e921bde36552e34564c5312bbc9e2
philogy <philippe@dumo.net>
2021-11-19 19:57:42 +0100

    Update to solidity 0.7

--- contracts/interfaces/IBank.sol
+++ contracts/interfaces/IBank.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.10;
+pragma solidity 0.7.0;
 
 interface IBank {
     struct Account { // Note that token values have an 18 decimal precision
--- contracts/mock/TestERC20.sol
+++ contracts/mock/TestERC20.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.10;
+pragma solidity ^0.7.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestERC20 is ERC20 {
-    constructor() ERC20("Some Test token", "TST") { }
+
+    // solhint-disable-next-line no-empty-blocks
+    constructor() ERC20("Some Test token", "TST") {}
 
     function mint(address _recipient, uint256 _amount) external {
         _mint(_recipient, _amount);
